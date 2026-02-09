00972e211658fb09292e107541438c89cbc39c6b
ppedvLukasK <lukask@ppedv.de>
2026-02-09 16:04:02 +0100

    M004

---
+++ M004/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace M004;
+
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		//Explicit Binding Update
+		TB.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+	}
+}
---
+++ M004/ScoreToGradeConverter.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace M004;
+
+class ScoreToGradeConverter : IValueConverter
+{
+	/// <summary>
+	/// Quelle zu Ziel
+	/// Slider zu TextBlock
+	/// double zu string
+	/// </summary>
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		double d = (double) value; //object zu double konvertieren
+		int i = (int) d;
+
+		switch (i)
+		{
+			case <= 50: return $"Ungenügend ({i})";
+			case <= 60: return $"Mangelhaft ({i})";
+			case <= 70: return $"Ausreichend ({i})";
+			case <= 80: return $"Befriedigend ({i})";
+			case <= 90: return $"Gut ({i})";
+			default: return $"Sehr Gut ({i})";
+		}
+	}
+
+	/// <summary>
+	/// Ziel zu Quelle
+	/// TextBlock zu Slider
+	/// Muss nicht implementiert werden
+	/// </summary>
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		return 0;
+	}
+}
