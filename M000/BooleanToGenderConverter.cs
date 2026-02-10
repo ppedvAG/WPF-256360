1c5c0d236acc5e8aa5cd6938429d74a9ef91d9f1
ppedvLukasK <lukask@ppedv.de>
2026-02-10 11:02:35 +0100

    M006

---
+++ M000/BooleanToGenderConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace M000;
+
+class BooleanToGenderConverter : IValueConverter
+{
+	//Backend -> Frontend (Person Objekt zu RadioButton)
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		Geschlecht v = (Geschlecht)value;
+		Geschlecht p = (Geschlecht)parameter;
+		return v == p;
+	}
+
+	//Frontend -> Backend (RadioButton -> Person)
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		bool b = (bool) value;
+		Geschlecht g = (Geschlecht) parameter;
+
+		if (b == true)
+			return g;
+		else
+			return Binding.DoNothing;
+	}
+}
