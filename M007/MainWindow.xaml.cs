b7a6e207ccd6639f890938747ee6eecb40161c49
ppedvLukasK <lukask@ppedv.de>
2026-02-10 13:18:36 +0100

    M007

---
+++ M007/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace M007;
+
+public partial class MainWindow : Window
+{
+	public ObservableCollection<Fahrzeug> Fahrzeuge { get; set; } =
+	[
+		new Fahrzeug(251, FahrzeugMarke.BMW),
+		new Fahrzeug(274, FahrzeugMarke.BMW),
+		new Fahrzeug(146, FahrzeugMarke.BMW),
+		new Fahrzeug(208, FahrzeugMarke.Audi),
+		new Fahrzeug(189, FahrzeugMarke.Audi),
+		new Fahrzeug(133, FahrzeugMarke.VW),
+		new Fahrzeug(253, FahrzeugMarke.VW),
+		new Fahrzeug(304, FahrzeugMarke.BMW),
+		new Fahrzeug(151, FahrzeugMarke.VW),
+		new Fahrzeug(250, FahrzeugMarke.VW),
+		new Fahrzeug(217, FahrzeugMarke.Audi),
+		new Fahrzeug(125, FahrzeugMarke.Audi)
+	];
+
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		Button b = sender as Button;
+		Fahrzeug f = b.DataContext as Fahrzeug;
+		Fahrzeuge.Remove(f);
+	}
+}
---
+++ M007/MarkeToPathConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace M007;
+
+class MarkeToPathConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		FahrzeugMarke m = (FahrzeugMarke)value;
+		string pathRoot = @"C:\Users\lk3\source\repos\WPF_2026_02_09\M007\Images\";
+		return pathRoot + m.ToString() + ".png";
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
