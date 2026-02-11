befad4a33e214c5e93923d88bc77f416f36c8f5e
ppedvLukasK <lukask@ppedv.de>
2026-02-11 11:00:50 +0100

    M010

---
+++ M010/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+namespace M010;
+
+public partial class MainWindow : Window
+{
+	public bool TwoOfFour => new bool[] { CB1Checked, CB2Checked, CB3Checked, CB4Checked }.Count(e => e == true) >= 2;
+
+	public bool CB1Checked { get; }
+
+	public bool CB2Checked { get; }
+
+	public bool CB3Checked { get; }
+
+	public bool CB4Checked { get; }
+
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+}
